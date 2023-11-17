f3c1aef792f81b25f4d6bb0733bea8362e8418f6
@hexlet/cli <support@hexlet.io>
2023-11-17 23:01:32 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -18,6 +18,25 @@ public class App {
             variables.put(key, value);
         }
 
+        Pattern envPattern = Pattern.compile("environment=\"(.+?)\"");
+        Matcher envMatcher = envPattern.matcher(configFileContent);
+
+        while (envMatcher.find()) {
+            String envList = envMatcher.group(1);
+            String[] envVariables = envList.split(",");
+            for (String envVariable : envVariables) {
+                String[] parts = envVariable.split("=");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    if (key.startsWith("X_FORWARDED_")) {
+                        key = key.substring(12);
+                        variables.put(key, value);
+                    }
+                }
+            }
+        }
+
         return variables.entrySet().stream()
                 .map(entry -> entry.getKey() + "=" + entry.getValue())
                 .collect(Collectors.joining(","));
