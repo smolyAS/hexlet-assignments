1a82d8414c23f6e040b6c6fa2e7bd5ee74d89eaf
@hexlet/cli <support@hexlet.io>
2023-11-17 22:31:40 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,27 +1,20 @@
 package exercise;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class App {
-
     public static String getForwardedVariables(String configFileContent) {
-        return Arrays.stream(configFileContent.split("\n"))
-                .filter(line -> line.trim().startsWith("environment"))
-                .map(line -> {
-                    String variables = line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""));
-                    return Arrays.stream(variables.split(","))
-                            .filter(pair -> pair.trim().startsWith("X_FORWARDED_"))
-                            .map(pair -> {
-                                String[] keyValue = pair.trim().split("=");
-                                String name = keyValue[0].substring(keyValue[0]
-                                        .indexOf("_") + 1)
-                                        .replace("FORWARDED_", "");
-                                String value = keyValue[1];
-                                return name + value;
-                            })
-                            .collect(Collectors.joining(","));
-                })
+        Map<String, String> variables = new HashMap<>();
+        Arrays.stream(configFileContent.split(","))
+                .filter(s -> s.startsWith("X_FORWARDED_"))
+                .map(s -> s.split("="))
+                .forEach(entry -> variables.put(entry[0].replace("X_FORWARDED_", ""), entry[1]));
+
+        return variables.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
                 .collect(Collectors.joining(","));
     }
 }
