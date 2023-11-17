c8876b682a7997827b22fae6d7d5a48d85466d03
@hexlet/cli <support@hexlet.io>
2023-11-17 22:39:15 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,20 +1,25 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class App {
     public static String getForwardedVariables(String configFileContent) {
         Map<String, String> variables = new HashMap<>();
-        Arrays.stream(configFileContent.split(","))
-                .filter(s -> s.startsWith("X_FORWARDED_"))
-                .map(s -> s.split("="))
-                .forEach(entry -> variables.put(entry[0].replace("X_FORWARDED_", ""), entry[1]));
+        Pattern pattern = Pattern.compile("\\bX_FORWARDED_(\\w+)=(\\w+)\\b");
+        Matcher matcher = pattern.matcher(configFileContent);
+
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+            variables.put(key, value);
+        }
 
         return variables.entrySet().stream()
-                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .map(entry -> entry.getKey().toUpperCase() + "=" + entry.getValue())
                 .collect(Collectors.joining(","));
     }
 }
