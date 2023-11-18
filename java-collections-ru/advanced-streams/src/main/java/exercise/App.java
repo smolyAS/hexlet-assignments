95a8e229b5367bcd9c28c53dec59f8c8de764c12
@hexlet/cli <support@hexlet.io>
2023-11-18 07:33:47 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class App {
+
     public static String getForwardedVariables(String configFile) {
         return Arrays.stream(configFile.split("\n"))
-                .filter(line -> line.startsWith("environment\""))
-                .map(line -> line.substring("environment\"".length()))
-                .flatMap(variables -> Arrays.stream(variables.split(",")))
+                .filter(line -> line.startsWith("environment=\""))
+                .map(line -> line.substring("environment=\"".length(), line.length() - 1))
+                .flatMap(line -> Arrays.stream(line.split(",")))
                 .map(String::trim)
-                .filter(variable -> variable.startsWith("X_FORWARDED_"))
-                .map(variable -> variable.substring("X_FORWARDED_".length()))
+                .filter(line -> line.startsWith("X_FORWARDED_"))
                 .collect(Collectors.joining(","));
     }
 
     public static void main(String[] args) {
-        String configFile = "environment\"X_FORWARDED_MAILtirion@google.com,X_FORWARDED_HOME/home/tirion,languageen\"";
+        String configFile = "environment=\"X_FORWARDED_variable=value,   \"\nkey=value";
         String forwardedVariables = getForwardedVariables(configFile);
         System.out.println(forwardedVariables);
     }
