59742690dade17a196eb067189051eb6e3f22836
@hexlet/cli <support@hexlet.io>
2024-01-24 22:02:44 +0300

    submit java-oop-ru/inheritance

---
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Map;
+
+// BEGIN
+class Tag {
+    private String name;
+    private Map<String, String> attributes;
+
+    public Tag(String name, Map<String, String> attributes) {
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    public String stringifyAttributes() {
+        return attributes.keySet().stream()
+                .map(key -> {
+                    String value = attributes.get(key);
+                    return String.format(" %s=\"%s\"", key, value);
+                })
+                .collect(Collectors.joining(""));
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+// END
